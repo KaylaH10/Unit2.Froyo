9f9f9311d05e465abf58f16f013d41897af5d4c1
Nguyen Vo <vophannguyen@gmail.com>
2023-09-21 16:35:53 -0400

    Nguyen vo Push

--- froyo.js
+++ froyo.js
@@ -1,10 +1,21 @@
 // Prompt the user for Froyo
 const userInputFroyo = prompt(
-    "Please enter some froyo flavors separated by commas:",
-    "strawberry,chocolate,vanilla"
-  );
+  "Please enter some froyo flavors separated by commas:",
+  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+);
 
-  // Parse into an array of strings/flavors.
+// Parse into an array of strings/flavors.
 const froyoArray = userInputFroyo.split(",");
-
 console.log(froyoArray);
+let object = {};
+// let keys = Object.keys(object);
+for (const item of froyoArray) {
+  if (object[item]) {
+    // if they have that item +1
+    object[item] += 1;
+  } // if they dont have item
+  else {
+    object[item] = 1;
+  }
+}
+console.table(object);
